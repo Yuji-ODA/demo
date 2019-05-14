0ea16bc6e46bfeaa3db29d45ceb09011c075ceea
yuji <yuoda@yahoo.co.jp>
2019-05-14 19:52:24 +0900

    webjars

--- src/main/java/com/example/demo/GenericClass.java
+++ src/main/java/com/example/demo/GenericClass.java
@@ -2,9 +2,7 @@ package com.example.demo;
 
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
-import org.springframework.stereotype.Component;
 
-@Component
 @RequiredArgsConstructor
 @Value
 public class GenericClass<T> {
--- src/test/java/com/example/demo/app/BookControllerTest.java
+++ src/test/java/com/example/demo/app/BookControllerTest.java
@@ -1,13 +1,19 @@
 package com.example.demo.app;
 
 
+import com.example.demo.Preference;
+import com.example.demo.domain.repository.BookRepository;
+import com.example.demo.domain.service.BookService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,14 +24,26 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith({SpringExtension.class})
-@WebMvcTest(controllers = BookController.class, secure = false)
+@WebMvcTest(controllers = BookController.class)
+@WithMockUser
 class BookControllerTest {
 
     @Autowired
     MockMvc mvc;
 
+    Preference preference;
+
+    @MockBean
+    BookRepository bookRepository;
+
+    BookService bookService;
+
+//    @Autowired
+//    Preference preference;
+
     @BeforeEach
     void setUp() {
+        bookService = new BookService(bookRepository);
     }
 
     @Test
@@ -50,5 +68,27 @@ class BookControllerTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    @TestConfiguration
+    @EnableConfigurationProperties(Preference.class)
+    public static class TestConfig {
+        @Autowired
+        Preference preference;
+
+        @Autowired
+        BookRepository bookRepository;
+
+        @Bean
+        public BookService bookService() {
+            return new BookService(bookRepository);
+        }
+
+        @Bean
+        public Preference preference() {
+            return preference;
+        }
+
+
     }
 }
