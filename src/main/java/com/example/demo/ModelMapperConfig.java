f06d24cfdc469229ee4491cb8f6c6682a70c3137
yuji <ugYBAd0leb>
2022-02-03 19:30:15 +0900

    save

--- src/main/java/com/example/demo/ModelMapperConfig.java
+++ src/main/java/com/example/demo/ModelMapperConfig.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,10 +10,13 @@ import static org.modelmapper.config.Configuration.AccessLevel;
 
 @Configuration
 public class ModelMapperConfig {
+
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFullTypeMatchingRequired(true)
                 .setFieldMatchingEnabled(true)
                 .setFieldAccessLevel(AccessLevel.PRIVATE)
                 .setPropertyCondition(Conditions.isNotNull());
